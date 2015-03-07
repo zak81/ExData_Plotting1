3edbe322d46fdf34c852e39e1ffdf41797dcb9ae
zak81 <zakzak8111@gmail.com>
2015-03-06 20:43:51 -0800

    Fixed plot4

--- plot4.R
+++ plot4.R
@@ -8,6 +8,6 @@ with(energy_usage_s, plot(DateTime, Voltage, xlab = "datetime", ylab = "Voltage"
 with(energy_usage_s, plot(DateTime, Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering"))
 with(energy_usage_s, lines(DateTime, Sub_metering_2, type = "l", col = "red"))
 with(energy_usage_s, lines(DateTime, Sub_metering_3, type = "l", col = "blue"))
-legend("topright", lty = 1, box.col = "white", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty = 1, bty = "n", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 with(energy_usage_s, plot(DateTime, Global_reactive_power, xlab = "datetime", type = "l"))
 dev.off()
